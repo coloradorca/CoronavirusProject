10b0889b9c698c6df5bb6437daee22bbfbd1a817
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-05-19 18:22:21 -0600

    include GeoJSON file, create template for OpenLayers map

--- src/OpenLayers.js
+++ src/OpenLayers.js
@@ -1,35 +1,38 @@
 import React, { useRef, useEffect } from 'react';
-// import ol from 'ol';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import LayerTile from 'ol/layer/Tile.js';
+import { LayerTile } from 'ol/layer';
 import SourceOSM from 'ol/source/OSM.js';
-import VectorLayer from 'ol/layer/Vector';
+import VectorLayer from 'ol/source/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
-import XYZ from 'ol/source/XYZ';
+// import XYZ from 'ol/source/XYZ';
+// import { Fill, Stroke, Style, Text } from 'ol/style';
 
 export default function OpenLayers() {
   //create a react reference to the map
   let mapRef = useRef();
 
   useEffect(() => {
-    var raster = new TileLayer({
-      source: new XYZ({
-        attributions: 'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
-            'rest/services/World_Topo_Map/MapServer">ArcGIS</a>',
-        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/' +
-            'World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
-      })
-    });
     //instantiate the map
-  const map = new Map({
-    //set its target to the ref, ultimately displaying it in the correct div in return statement below
+    const map = new Map({
+      //set the target html element to render the map
       target: mapRef.current,
-
+      //add layers
       layers: [
-        raster
+        new TileLayer({
+          source: new SourceOSM(),
+        }),
+
+        // TODO:  Display the vector layer from the GeoJSON file
+
+        // new VectorLayer({
+        //   source: new VectorSource({
+        //     format: new GeoJSON(),
+        //     url: './data/countries.geojson',
+        //   }),
+        // }),
       ],
       //instantiate the view
       view: new View({
@@ -37,27 +40,7 @@ export default function OpenLayers() {
         zoom: 13,
       }),
     });
-
   }, []);
 
   return <div className='webmap' ref={mapRef} />;
-  // return <div>This is the OL map component</div>;
 }
-
-  //   const map = new Map({
-  //     target: mapRef.current,
-  //     layers: [
-  //       new VectorLayer({
-  //         source: new VectorSource({
-  //           format: new GeoJSON(),
-  //           url: './data/countries.geojson'
-  //         }),
-  //       }),
-  //     ],
-  //     view: new View({
-  //       center: [12, 40],
-  //       zoom: 2,
-  //     }),
-  //   });
-
-  // }, []);
