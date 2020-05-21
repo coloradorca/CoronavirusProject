811e7fe378a2a154b4ce4806a2cf5704b27a5565
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-05-21 10:38:47 -0600

    Size map circles, add new csv file

--- src/EsriMap.js
+++ src/EsriMap.js
@@ -17,34 +17,100 @@ export default function EsriMap({
         'esri/views/MapView',
         'esri/layers/FeatureLayer',
         'esri/widgets/Search',
+        "esri/widgets/Legend",
       ],
       { css: true },
-    ).then(([ArcGISMap, MapView, FeatureLayer, Search]) => {
-      const map = new ArcGISMap({
-        //change the basemap color/value here
-        basemap: 'topo',
-        layers: [],
-      });
+    ).then(([ArcGISMap, MapView, FeatureLayer, Search, Legend]) => {
 
-      //set the map's center point and zoom level
-      const view = new MapView({
-        container: mapRef.current,
-        map: map,
-        center: [-104, 40],
-        zoom: 6,
-      });
-
-      //set constraints on the zoom
-      view.constraints = {
-        minZoom: 2,
-        maxZoom: 7,
+      //define the symbol for each marker displayed on map
+      var defaultSym = {
+        type: "simple-marker",
+        color: [0, 0, 0, 0],
+        outline: {
+          color: "red",
+          width: 1
+        }
+      };
+      //create the breakpoints for the size of circles displayed
+      var renderer = {
+        type: "simple",
+        symbol: defaultSym,
+        visualVariables: [
+          {
+            type: "size",
+            field: "Confirmed",
+            legendOptions: {
+              title: "Amount of confirmed cases"
+            },
+            stops: [
+              {
+                value:1000,
+                size: 4,
+                label: "<1000"
+              },
+              {
+                value: 2000,
+                size: 10,
+                label: "10-20k"
+              },
+              {
+                value: 40000,
+                size: 15,
+                label: "20 - 40k"
+              },
+               {
+                value: 80000,
+                size: 25,
+                label: ">80,000",
+               }
+            ]
+          }
+        ]
       };
 
-      //featureLayer displaying red circles of coronavirus cases
+      //function to serve as the popuptitle (conditional for rendering Province/state if available)
+      const popupTitle = (feature) => {
+        if(feature.graphic.attributes.Province_State){
+          return `${feature.graphic.attributes.Province_State}, ${feature.graphic.attributes.Country_Region} `
+        } else {
+          return `${feature.graphic.attributes.Country_Region} `
+        }
+      }
+
+      //featureLayer and data generated from ArcGis Rest Service
       var covidLayer = new FeatureLayer({
         url:
           'https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1',
+        renderer: renderer,
+        title: "Coronavirus Cases",
+        popupTemplate: {
+          title: popupTitle,
+          content:
+            "{Deaths} people have died of {Confirmed} cases",
+        },
         outFields: ['*'],
+      });
+
+      const map = new ArcGISMap({
+        //change the basemap color/value here
+        basemap: 'dark-gray',
+        //add featureLayers to the map
+        layers: [
+          covidLayer,
+                  // covid2Layer
+        ]
+      });
+
+      //set the map's center point, zoom level/constraints, HTML output
+      const view = new MapView({
+        container: mapRef.current,
+        map: map,
+        center: [-85.0502, 33.125524],
+          zoom: 3,
+        constraints : {
+          minZoom: 2,
+          maxZoom: 7,
+        }
       });
 
       //add search bar
@@ -55,6 +121,14 @@ export default function EsriMap({
       view.ui.add(search, {
         position: 'top-right',
       });
+
+      // add a map legend
+      // view.ui.add(
+      //   new Legend({
+      //     view: view
+      //   }),
+      //   "bottom-left"
+      // );
 
       //click handler to select country
       view.on('click', function (event) {
@@ -72,16 +146,13 @@ export default function EsriMap({
             })[0].graphic;
             //get information from the selected country
             //set the country clicked back up to main App component
+            console.log(graphic.attributes)
             changeNation(graphic.attributes.Country_Region);
             changeStatistics(graphic.attributes);
             toggleChart();
           }
         });
       });
-
-
-        map.add(covidLayer, 0);
-
 
       return () => {
         if (view) {
